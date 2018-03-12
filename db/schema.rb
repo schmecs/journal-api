b971c6e3c912d031bb8477a37dccd16b5429519e
Rebecca <rblum@patientslikeme.com>
2018-03-12 07:12:50 -0400

    Add email and password digest to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302122128) do
+ActiveRecord::Schema.define(version: 20180312110903) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180302122128) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
